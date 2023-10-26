0c8f18dea22be8f8c8eae2912ed217e132dd2378
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-26 06:13:27 +0300

    Select elements

--- meal.js
+++ meal.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {   
-
+  const wrapper = document.querySelector('.wrapper');
+  const categoryBtn = document.querySelector('.categoryBtn');
 
   
     
