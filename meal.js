b9fa4e5cda0c61a38077c73c0695f5f162bfcdde
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-26 06:13:51 +0300

    fetch Data

--- meal.js
+++ meal.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const wrapper = document.querySelector('.wrapper');
   const categoryBtn = document.querySelector('.categoryBtn');
 
-  
+  fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(res => res.json())
+    .then(data => {
+      const results = data.categories;
+
+    
+    })
     
 });
